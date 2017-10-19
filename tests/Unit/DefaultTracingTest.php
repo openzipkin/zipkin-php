a2e78c83c267f214ae82eb183202fa6539c27f24
José Carlos Chávez <jcchavezs@gmail.com>
2017-10-20 00:16:19 +0200

    Adds test for DefaultTracingTest.

---
+++ tests/Unit/DefaultTracingTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ZipkinTests\Unit;
+
+use PHPUnit_Framework_TestCase;
+use Zipkin\DefaultTracing;
+use Zipkin\Endpoint;
+use Zipkin\Propagation\Propagation;
+use Zipkin\Reporters\NoopLogging;
+use Zipkin\Samplers\BinarySampler;
+use Zipkin\Tracer;
+use Zipkin\Tracing;
+
+final class DefaultTracingTest extends PHPUnit_Framework_TestCase
+{
+    public function testDefaultTracingCreationSuccess()
+    {
+        $localEndpoint = Endpoint::createAsEmpty();
+        $reporter = new NoopLogging();
+        $sampler = BinarySampler::createAsNeverSample();
+        $isNoop = $this->randomBool();
+
+        $tracing = new DefaultTracing($localEndpoint, $reporter, $sampler, $isNoop);
+
+        $this->assertInstanceOf(Tracing::class, $tracing);
+        $this->assertInstanceOf(Tracer::class, $tracing->getTracer());
+        $this->assertInstanceOf(Propagation::class, $tracing->getPropagation());
+    }
+
+    private function randomBool()
+    {
+        return (mt_rand(0, 1) === 1);
+    }
+}
