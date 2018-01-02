ebff088fae1dca4b5af81ec794d567cb80ac67a5
José Carlos Chávez <jcchavezs@gmail.com>
2018-01-02 15:19:22 +0100

    [#27] Improves the CurlFactory.

--- src/Zipkin/Reporters/Http/CurlFactory.php
+++ src/Zipkin/Reporters/Http/CurlFactory.php
@@ -2,19 +2,24 @@
 
 namespace Zipkin\Reporters\Http;
 
+use BadFunctionCallException;
 use RuntimeException;
 
-class CurlFactory
+final class CurlFactory implements ClientFactory
 {
     public static function create()
     {
+        if (!function_exists('curl_init')) {
+            throw new BadFunctionCallException('cURL is not enabled');
+        }
+
         return new self();
     }
 
     /**
      * {@inheritdoc}
      */
-    public function build(array $options)
+    public function build(array $options = [])
     {
         /**
          * @param string $payload
@@ -23,10 +28,8 @@ class CurlFactory
          */
         return function ($payload) use ($options) {
             $handle = curl_init($options['endpoint_url']);
-
-            curl_setopt($handle, CURLOPT_CUSTOMREQUEST, 'POST');
+            curl_setopt($handle, CURLOPT_POST, 1);
             curl_setopt($handle, CURLOPT_POSTFIELDS, $payload);
-            curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($handle, CURLOPT_HTTPHEADER, [
                 'Content-Type: application/json',
                 'Content-Length: ' . strlen($payload),
@@ -34,17 +37,20 @@ class CurlFactory
 
             if (curl_exec($handle) === true) {
                 $statusCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+                curl_close($handle);
 
-                if ($statusCode !== '202') {
-                    throw new RuntimeException(sprintf('Reporting of spans failed, code %s', $statusCode));
+                if ($statusCode !== 202) {
+                    throw new RuntimeException(
+                        sprintf('Reporting of spans failed, status code %d', $statusCode)
+                    );
                 }
             } else {
-                throw new RuntimeException(
-                    sprintf('Reporting of spans failed: %s, code %s', curl_error($handle), curl_errno($handle))
-                );
+                throw new RuntimeException(sprintf(
+                    'Reporting of spans failed: %s, error code %s',
+                    curl_error($handle),
+                    curl_errno($handle)
+                ));
             }
-
-            curl_close($handle);
         };
     }
 }
