6987a5361b79ea9bee34b34cf2eea94f6607dd8f
José Carlos Chávez <jcchavezs@gmail.com>
2018-01-23 21:56:27 +0100

    Adds tests for Ids.

---
+++ src/Zipkin/Propagation/Id.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Zipkin\Propagation\Id;
+
+/**
+ * @return string
+ */
+function generateTraceIdWith128bits()
+{
+    return bin2hex(openssl_random_pseudo_bytes(16));
+}
+
+/**
+ * @return string
+ */
+function generateNextId()
+{
+    return bin2hex(openssl_random_pseudo_bytes(8));
+}
+
+/**
+ * @param string $value
+ * @return bool
+ */
+function isValidTraceId($value)
+{
+    return ctype_xdigit((string) $value) &&
+        (strlen((string) $value) === 16 || strlen((string) $value) === 32);
+}
+
+/**
+ * @param string $value
+ * @return bool
+ */
+function isValidSpanId($value)
+{
+    return ctype_xdigit((string) $value) && strlen((string) $value) === 16;
+}
