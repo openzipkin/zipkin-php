17a23b99acb7905e747f5f6a4549d3e256ff34b3
José Carlos Chávez <jcchavezs@gmail.com>
2020-04-20 10:32:00 +0200

    chore: makes lookup for Map getter to be case insensitive (#137).

--- src/Zipkin/Propagation/Map.php
+++ src/Zipkin/Propagation/Map.php
@@ -12,6 +12,13 @@ final class Map implements Getter, Setter
 {
     /**
      * {@inheritdoc}
+     *
+     * If the carrier is an array, the lookup is case insensitive, this is
+     * mainly because Map getter is commonly used for those cases where the
+     * HTTP framework does not follow the PSR request/response objects (e.g.
+     * Symfony) and thus the header bag should be treated as a map. ArrayAccess
+     * can't be case insensitive because we can not know the keys on beforehand.
+     *
      * @param array|ArrayAccess $carrier
      */
     public function get($carrier, string $key): ?string
@@ -23,7 +30,12 @@ final class Map implements Getter, Setter
         }
 
         if (\is_array($carrier)) {
-            return \array_key_exists($lKey, $carrier) ? $carrier[$lKey] : null;
+            if (empty($carrier)) {
+                return null;
+            }
+
+            $lcCarrier = \array_change_key_case($carrier, \CASE_LOWER);
+            return \array_key_exists($lKey, $lcCarrier) ? $lcCarrier[$lKey] : null;
         }
 
         throw InvalidPropagationCarrier::forCarrier($carrier);
@@ -39,6 +51,8 @@ final class Map implements Getter, Setter
             throw InvalidPropagationKey::forEmptyKey();
         }
 
+        // Lowercasing the key was a first attempt to be compatible with the
+        // getter when using the Map getter for HTTP headers.
         $lKey = \strtolower($key);
 
         if ($carrier instanceof ArrayAccess || \is_array($carrier)) {
