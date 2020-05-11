ad492ab5d4a61693a06d75799149015e6c49ff94
José Carlos Chávez <jcchavezs@gmail.com>
2020-05-12 01:09:19 +0200

    chore: fixes retrocompatibility of the HTTP reporter constructor (#142)
    * chore: fixes retrocompatibility of the HTTP reporter constructor
    
    * chore: fix PHPStan complaints.
    
    * docs: fixes HTTP reporter setup.

--- tests/Unit/Reporters/HttpTest.php
+++ tests/Unit/Reporters/HttpTest.php
@@ -2,6 +2,7 @@
 
 namespace ZipkinTests\Unit\Reporters;
 
+use TypeError;
 use Zipkin\Endpoint;
 use Prophecy\Argument;
 use Zipkin\Recording\Span;
@@ -9,16 +10,42 @@ use Zipkin\Reporters\Http;
 use Psr\Log\LoggerInterface;
 use PHPUnit\Framework\TestCase;
 use Zipkin\Propagation\TraceContext;
+use Zipkin\Reporters\Http\CurlFactory;
 
 final class HttpTest extends TestCase
 {
     const PAYLOAD = '[{"id":"%s","name":null,"traceId":"%s","parentId":null,'
         . '"timestamp":null,"duration":null,"debug":false,"localEndpoint":{"serviceName":""}}]';
 
-    public function testCreateHttpReporterWithDefaultDependencies()
+    public function testConstructorIsRetrocompatible()
     {
-        $httpReporter = new Http();
-        $this->assertInstanceOf(Http::class, $httpReporter);
+        $this->assertInstanceOf(Http::class, new Http());
+
+        // old constructor
+        $this->assertInstanceOf(Http::class, new Http(CurlFactory::create()));
+        $this->assertInstanceOf(Http::class, new Http(
+            CurlFactory::create(),
+            ['endpoint_url' => 'http://myzipkin:9411/api/v2/spans',]
+        ));
+
+        // new constructor
+        $this->assertInstanceOf(Http::class, new Http(
+            ['endpoint_url' => 'http://localhost:9411/api/v2/spans']
+        ));
+        $this->assertInstanceOf(Http::class, new Http(
+            ['endpoint_url' => 'http://localhost:9411/api/v2/spans'],
+            CurlFactory::create()
+        ));
+
+        try {
+            new Http(null);
+            $this->fail('Expected the constructor to fail.');
+        } catch (TypeError $e) {
+            $this->assertEquals(
+                'Argument 1 passed to Zipkin\Reporters\Http::__construct must be of type array, null given',
+                $e->getMessage()
+            );
+        }
     }
 
     public function testHttpReporterSuccess()
