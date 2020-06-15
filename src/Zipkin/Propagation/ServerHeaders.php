c84ece74a04553090f14f0f0f49f1b04ddf3f87f
Daniele <danielerolando90@gmail.com>
2020-06-15 01:01:50 -0700

    Support extracting incoming headers from the $_SERVER variable (#156)
    * Support extracting incoming headers from the $_SERVER variable
    
    * chore: removes composer update instruction from tests
    
    * docs: changes comments semantic into multiline one.
    
    Co-authored-by: José Carlos Chávez <jcchavezs@gmail.com>

---
+++ src/Zipkin/Propagation/ServerHeaders.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zipkin\Propagation;
+
+/**
+ *
+ * This class implements the Zipkin\Propagation\Getter interface to extract the zipkin
+ * headers out of the $_SERVER variable.
+ * Due to the Getter::get signature, $_SERVER gets passed in as $carrier rather
+ * than get accessed here directly.
+ *
+ * Example:
+ *   $extractor = $this->tracing->getPropagation()->getExtractor(new ServerHeaders);
+ *   $extractedContext = $extractor($_SERVER);
+*/
+final class ServerHeaders implements Getter
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @param mixed $carrier
+     * @param string $key
+     * @return string|null
+     */
+    public function get($carrier, string $key): ?string
+    {
+        // Headers in $_SERVER are always uppercased, with any - replaced with an _
+        $key = strtoupper($key);
+        $key = str_replace('-', '_', $key);
+
+        return $carrier['HTTP_' . $key] ?? null;
+    }
+}
