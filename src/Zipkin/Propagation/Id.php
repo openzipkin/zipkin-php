12c79425336596f9911443f3cb9dfe5a70fa7fec
Matt Rink <matt.rink@futurenet.com>
2021-01-26 16:02:06 +0000

    Reverted change to Id.php as it's issue is ignored in phpstan.neon.

--- src/Zipkin/Propagation/Id.php
+++ src/Zipkin/Propagation/Id.php
@@ -6,31 +6,17 @@ namespace Zipkin\Propagation\Id;
 
 /**
  * @return string
- * @throws \RuntimeException
  */
 function generateTraceIdWith128bits(): string
 {
-    $pseudoBytes = \openssl_random_pseudo_bytes(16);
-
-    if ($pseudoBytes === false) {
-        throw new \RuntimeException("Unable to generate a pseudo-random byte string.");
-    }
-
-    return \bin2hex($pseudoBytes);
+    return \bin2hex(\openssl_random_pseudo_bytes(16));
 }
 
 /**
  * @return string
- * @throws \RuntimeException
  */
 function generateNextId(): string
 {
-    $pseudoBytes = \openssl_random_pseudo_bytes(16);
-
-    if ($pseudoBytes === false) {
-        throw new \RuntimeException("Unable to generate a pseudo-random byte string.");
-    }
-
     return \bin2hex(\openssl_random_pseudo_bytes(8));
 }
 
